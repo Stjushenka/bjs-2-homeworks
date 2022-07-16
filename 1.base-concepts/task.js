809d9b9a108d5a53b109fc27a60cc5d3ac23fb2e
unknown <stjushenka@gmail.com>
2022-07-16 15:50:36 +0300

    go

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,35 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+
+  let arr = [];
+  let discriminant = b**2-4*a*c;// код для задачи №1 писать здесь
+  if (discriminant === 0) {
+    arr.push(- b / (2 * a));
+  }
+ else if (discriminant > 0) {
+  arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+  arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  } 
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let S = amount - contribution;
+  let P = percent/100/12;
+  
+
+  let dateFrom = new Date();                                                                  
+  let fullMonthsDataTo = date.getFullYear() * 12 ;              
+  let fullMonthsDataFrom = dateFrom.getFullYear() * 12;           
+  let n = fullMonthsDataTo - fullMonthsDataFrom;                  
+  let Платеж = S * (P + (P / (((1 + P)**n) - 1)));
+  totalAmount = (Платеж * n).toFixed(2);
+
 
   // код для задачи №2 писать здесь
 
   return totalAmount;
 }
+
+
